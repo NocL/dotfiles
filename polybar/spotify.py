16606dbece110d48835245ecee87f3359eb1750b
NocL <ws2nocl@gmail.com>
2018-05-19 08:46:19 +0900

    spotify script added

---
+++ polybar/spotify.py
@@ -0,0 +1,44 @@
+#!/bin/python
+
+import sys
+import dbus
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-t', '--trunclen', type=int, metavar='trunclen')
+args = parser.parse_args()
+
+if args.trunclen is not None:
+        trunclen = args.trunclen
+else:
+        trunclen = 25
+
+try:
+        session_bus = dbus.SessionBus()
+        spotify_bus = session_bus.get_object("org.mpris.MediaPlayer2.spotify", "/org/mpris/MediaPlayer2")
+
+        spotify_properties = dbus.Interface(spotify_bus, "org.freedesktop.DBus.Properties")
+
+        metadata = spotify_properties.Get("org.mpris.MediaPlayer2.Player", "Metadata")
+
+        artist = metadata['xesam:artist'][0]
+        song = metadata['xesam:title']
+
+        if len(song) > trunclen:
+            song = song[0:trunclen]
+            song += '...' 
+            if ('(' in song) and (')' not in song):
+                song += ')'
+        output = artist + '  /  ' + song
+                                                                                                
+        if sys.version_info.major == 3:
+            print(output)
+        else:
+            print(output.encode('UTF-8'))
+except Exception as e:
+        if isinstance(e, dbus.exceptions.DBusException):
+            print("")
+        else:
+            print(e)
+                                                
